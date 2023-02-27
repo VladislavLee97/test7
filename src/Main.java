3d15fbed992735db74a3969ed57bdfbcf1a75948
Vladislav <rk92vlan554@gmail.com>
2023-02-27 11:36:51 +0500

    Исправил замечание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Тренируюсь создавать коммит и пушить в гитхаб");
+        System.out.println("Тренируюсь");
     }
 }
